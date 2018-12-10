aa90553a0237bd25a1702720412357eeae2bc5f5
Evan Guirino <etg624@gmail.com>
2018-12-10 13:43:40 -0800

    added some store functions

--- scripts/shopping-list.js
+++ scripts/shopping-list.js
@@ -57,12 +57,12 @@ const shoppingList = (function(){
   
   function addItemToShoppingList(itemName) {
     try {
-      Item.validateName(itemName);
+      Item.validateName()
       store.items.push(Item.create(itemName));
-      render();
     } catch (error) {
-      console.log(`Cannot add item ${error.message}`);
+      console.log(`Cannot ca item ${error.message}`)
     }
+    render();
   }
   
   function handleNewItemSubmit() {
@@ -75,10 +75,7 @@ const shoppingList = (function(){
     });
   }
   
-  function toggleCheckedForListItem(id) {
-    const foundItem = store.items.find(item => item.id === id);
-    foundItem.checked = !foundItem.checked;
-  }
+ 
   
   
   function getItemIdFromElement(item) {
@@ -90,20 +87,12 @@ const shoppingList = (function(){
   function handleItemCheckClicked() {
     $('.js-shopping-list').on('click', '.js-item-toggle', event => {
       const id = getItemIdFromElement(event.currentTarget);
-      toggleCheckedForListItem(id);
+      store.findAndToggleChecked(id);
       render();
     });
   }
+
   
-  function deleteListItem(id) {
-    const index = store.items.findIndex(item => item.id === id);
-    store.items.splice(index, 1);
-  }
-  
-  function editListItemName(id, itemName) {
-    const item = store.items.find(item => item.id === id);
-    item.name = itemName;
-  }
   
   function toggleCheckedItemsFilter() {
     store.hideCheckedItems = !store.hideCheckedItems;
@@ -120,7 +109,7 @@ const shoppingList = (function(){
       // get the index of the item in store.items
       const id = getItemIdFromElement(event.currentTarget);
       // delete the item
-      deleteListItem(id);
+      store.findAndDelete(id);
       // render the updated shopping list
       render();
     });
@@ -131,7 +120,7 @@ const shoppingList = (function(){
       event.preventDefault();
       const id = getItemIdFromElement(event.currentTarget);
       const itemName = $(event.currentTarget).find('.shopping-item').val();
-      editListItemName(id, itemName);
+      store.findAndUpdateName(id, itemName);
       render();
     });
   }
--- scripts/store.js
+++ scripts/store.js
@@ -3,44 +3,59 @@
 
 // eslint-disable-next-line no-unused-vars
 const store = (function () {
-  const items = [
-    { id: cuid(), name: 'apples', checked: false },
-    { id: cuid(), name: 'oranges', checked: false },
-    { id: cuid(), name: 'milk', checked: true },
-    { id: cuid(), name: 'bread', checked: false }
-  ];
-  const hideCheckedItems = false;
-  const searchTerm = '';
-  const findById = function(id) {
-    items.find(item => item.id === id);
-  };
-  const addItem = function(name) {
-    try {
-      this.items.push(Item.create(name));
+    const items = [
+        { id: cuid(), name: 'apples', checked: false },
+        { id: cuid(), name: 'oranges', checked: false },
+        { id: cuid(), name: 'milk', checked: true },
+        { id: cuid(), name: 'bread', checked: false }
+    ];
+    const hideCheckedItems = false;
+    const searchTerm = '';
+    const findById = function (id) {
+        return items.find(item => item.id === id);
+
+    };
+    const addItem = function (name) {
+        try {
+            this.items.push(Item.create(name));
+        }
+        catch (error) {
+            console.log(`Cannot add item ${error.message}`);
+        }
+    };
+    const findAndToggleChecked = function (id) {
+        const foundItem = this.findById(id);
+        foundItem.checked = !foundItem.checked;
+    };
+    const findAndUpdateName = function (id, newName) {
+        try {
+            const foundId = this.findById(id);
+            foundId.name = newName;
+        }
+        catch (error) {
+            console.log(`Cannot update name: ${error.message}`);
+        }
+    };
+    const findAndDelete = function (id) {
+        const foundID = this.findById(id)
+        this.items.splice(foundID, 1);
+    };
+    const toggleCheckedFilter = function () {
+        return this.hideCheckedItems = !this.hideCheckedItems;
     }
-    catch(error) {
-      console.log(`Cannot add item ${error.message}`);
+    const setSearchTerm = function (term) {
+        return this.searchTerm = term;
     }
-  };
-  const findAndToggleChecked = function (id) {
-    const foundItem = this.findByID(id);
-    foundItem.checked = !foundItem.checked;
-  };
-  const findAndUpdateName = function(id, newName) {
-    try {
-      const foundId = this.findByID(id);
-      foundId.name = newName;
-    }
-    catch(error) {
-      console.log(`Cannot update name: ${error.message}`);
-    }
-  };
-  const findAndDelete = function(id) {
-    const foundID = this.findById(id)
-    this.items.splice(foundID, 1);
-  };
-  return {
-    items, hideCheckedItems, searchTerm, findById, addItem, findAndToggleChecked, findAndUpdateName, findAndDelete
-  };
-     
-}() );
+    return {
+        items, 
+        hideCheckedItems, 
+        searchTerm, 
+        findById, 
+        addItem, 
+        findAndToggleChecked, 
+        findAndUpdateName, 
+        findAndDelete,
+        toggleCheckedFilter
+    };
+
+}());
