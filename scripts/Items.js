b518dd87997c8c0d9e493cad899cbb16721a50fa
Eugene Song <yougene42193@gmail.com>
2018-12-10 14:24:37 -0500

    namespace step 1

--- scripts/Items.js
+++ scripts/Items.js
@@ -1,11 +1,23 @@
 /* global shoppingList, cuid, item */
 'use strict';
 
-
-
 const Item = (function () {
+  const validateName = function (name) {
+    if (!name) {
+      throw new TypeError ('Name does not exist');
+      
+    }
+  };
+  const create = function (name) {
+    return {
+      id: cuid(),
+      name: name,
+      check: 'false',
+    };
+  };
   return {
-    
-  }
+    validateName,
+    create
+  };
 }());
 
--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,16 @@
 
 $(document).ready(function() {
   shoppingList.bindEventListeners();
-  shoppingList.render();
+  const itemNames = [ '', 'apples', 'pears' ];
+itemNames.forEach(name => {
+  try {
+    Item.validateName(name);
+    store.items.push(Item.create(name));
+  } catch(error) {
+    console.log('Cannot add item: ' + error.message);
+  }
 });
+shoppingList.render();
 
+});
 
