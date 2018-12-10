05a8108c119889242f6600aba4b8ef8d1f6de7ac
Eugene Song <yougene42193@gmail.com>
2018-12-10 12:13:27 -0500

    finished step one

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+/* global shoppingList, cuid */
+
+// eslint-disable-next-line no-unused-vars
+
+$(document).ready(function() {
+  shoppingList.bindEventListeners();
+  shoppingList.render();
+});
+
+console.log(store);
